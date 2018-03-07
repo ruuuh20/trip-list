a3c0d2152a5dd739f44bf12281b22b414387259f
ruuuh20 <pkim2320@gmail.com>
2018-03-07 18:23:32 -0500

    added attractions crud

--- app/controllers/api/attractions_controller.rb
+++ app/controllers/api/attractions_controller.rb
@@ -1,4 +1,5 @@
 class Api::AttractionsController < ApplicationController
+  before_action :set_attraction, only: [:show, :destroy, :edit]
 
   def index
     render json: Attraction.all
@@ -14,10 +15,35 @@ class Api::AttractionsController < ApplicationController
   end
 
   def show
-    render json: Attraction.find_by(id: params[:id])
+    if @attraction
+      render json: @attraction
+    else
+      render json: { message: "Not found" }, status: 404
+    end
+  end
+
+  def update
+    if @attraction.update(attraction_params)
+      render json: @attraction
+    else
+      render json: { message: @attraction.errors }, status: 400
+    end
+  end
+
+  def destroy
+    if @attraction.destroy
+      render json: { message: "Deleted" }, status: 204
+    else
+      render json: { message: "Unable to remove" }, status: 400
+    end
   end
 
   private
+  #helper
+    def set_attraction
+      @attraction = Attraction.find_by(id: params[:id])
+    end
+
     def attraction_params
       params.require(:attraction).permit(:name, :location, :img_url, :price, :website)
     end
