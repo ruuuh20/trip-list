49279759306f5be5954e3938b00b5d8a58052e27
ruuuh20 <pkim2320@gmail.com>
2018-03-10 00:32:42 -0500

    add trips component and style

--- client/src/components/trips.js
+++ client/src/components/trips.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './trips.css'
 
 const Trips = (props) => (
   <div>
     {props.trips.map(trip =>
-      <div>
+      <div className="tripCard">
         <h3>{trip.name}</h3>
       </div>
     )}
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// import { createStore } from 'redux';
+// import thunk from 'redux-thunk';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
