9ed8a064aa46bf7dc203d4cc5f8655e7e09c93d0
ruuuh20 <pkim2320@gmail.com>
2018-03-10 14:13:57 -0500

    added trip show action in controller

--- client/src/components/TripCard.js
+++ client/src/components/TripCard.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const TripCard = ({trip}) => (
   <div key={trip.id} className="tripcard">
     <h1>{trip.destination}</h1>
+    <Link to={`/trips/${trip.id}`} className="trip-item">Show</Link>
   </div>
 )
 
