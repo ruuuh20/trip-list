306e377e84271ea064f05e1c04228f02af24db7c
ruuuh20 <pkim2320@gmail.com>
2018-03-09 19:14:21 -0500

    added trips component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
-
-
 import './App.css';
 import { NavBar } from './components/navbar'
+import Trips from './components/trips'
 
 class App extends Component {
   render() {
     return (
       <Router>
       <div className="App">
-      <NavBar>
-      </NavBar>
-        <header className="App-header">
-
-          <h1 className="App-title">Trip List App</h1>
-        </header>
-        <p className="App-intro">
-          Choose your trip.
-        </p>
-
+        <NavBar/>
+        <h1 className="App-title">Trip List App</h1>
+        <p className="App-intro">Choose your trip.</p>
+        <Trips />
       </div>
       </Router>
     );
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const link = {
-  width: '100px',
-  padding: '12px',
-  margin: '0 6px 6px',
-  background: 'blue',
-  textDecoration: 'none',
-  color: 'white',
-}
+// const link = {
+//   width: '100px',
+//   padding: '12px',
+//   margin: '0 6px 6px',
+//   background: 'blue',
+//   textDecoration: 'none',
+//   color: 'white',
+// }
 
 
 export const NavBar = (props) => {
   return (
-    <NavLink
-     to="/about"
-     exact
-     style={link}
-     activeStyle={{
-       background: 'darkblue'
-     }}
-   >About</NavLink>
+    <div>
+      <div className="navbar">
+        <NavLink
+        to="/"
+        exact activeClassName="active"
+        >Home</NavLink>
+        <NavLink
+         to="/about"
+       >About</NavLink>
+     </div>
+    </div>
   )
 }
