5c73541c8bc25528b01bad92a85ae3880b1604aa
ruuuh20 <pkim2320@gmail.com>
2018-07-07 10:30:19 -0400

    going to fix and refactorrrrr

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AboutPage = (props) => {
   return (
     <div className="about">
-      <h2>Plan your vacation and make your trip wishlist</h2>
+      <h2>Plan your vacation and make your trip wishlist </h2>
       <p>Plan out your trip that you know you will go one day, or bookmark whenever you come across a place you want to visit in the future.</p>
 
     </div>
