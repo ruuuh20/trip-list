a8f9093c8338c781ed3e115e7a914bfad9b7d791
ruuuh20 <pkim2320@gmail.com>
2018-07-14 22:59:52 -0400

    fetch using jsonp and dropdown

--- client/src/containers/SearchContainer.js
+++ client/src/containers/SearchContainer.js
@@ -4,7 +4,8 @@ import { connect } from 'react-redux';
 import { updateSearch, storeSearch, retrieveSearches, clearHistory } from '../actions/search';
 import { fetchVenues } from '../actions/attractions';
 import { bindActionCreators } from 'redux';
-import './SearchContainer.css'
+import './SearchContainer.css';
+import fetchJsonp from 'fetch-jsonp';
 
 // import 'jquery/src/jquery';
 import $ from 'jquery';
@@ -14,14 +15,34 @@ import $ from 'jquery';
 class SearchContainer extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      countries: []
+    }
   }
 
   componentDidMount = () => {
-    console.log("HELLO")
+    let initialCountries = [];
+
+   fetchJsonp('https://battuta.medunes.net/api/country/all/?&key=bd8c940f6ad6dfa181f7d32922758c74&callback=?')
+   .then((response) => {
+  console.log(response)
+           return response.json();
+       }).then(data => {
+         // console.log(data.body + "Hellllllooooo")
+         // console.log(JSON.stringify(data))
+         var stringed = JSON.stringify(data)
+         console.log(data.length)
+
+       initialCountries = data.map((country) => {
+           return country.name
+       });
+       console.log(initialCountries);
+       this.setState({
+           countries: initialCountries,
+       });
+   });
 
   }
-
-  // componentDidMount
 
   handleOnChange = (event) => {
     this.props.updateSearch(event.target.value);
@@ -36,83 +57,7 @@ class SearchContainer extends Component {
 
 
 
-
-
-// componentDidMount = () => {
-//   $(document).ready(function(){
-//
-//
-//
-//   	 $("#f_elem_city").autocomplete({
-//   		source: function (request, response) {
-//   		 $.getJSON(
-//   			"http://gd.geobytes.com/AutoCompleteCity?callback=?&q="+request.term,
-//   			function (data) {
-//   			 response(data);
-//   			}
-//   		 );
-//   		},
-//   		minLength: 3,
-//   		select: function (event, ui) {
-//   		 var selectedObj = ui.item;
-//   		 $("#f_elem_city").val(selectedObj.value);
-//   		getcitydetails(selectedObj.value);
-//   		 return false;
-//   		},
-//   		open: function () {
-//   		 $(this).removeClass("ui-corner-all").addClass("ui-corner-top");
-//   		},
-//   		close: function () {
-//   		 $(this).removeClass("ui-corner-top").addClass("ui-corner-all");
-//   		}
-//   	 });
-//   	 $("#f_elem_city").autocomplete("option", "delay", 100);
-//   	});
-//
-//
-// function getcitydetails(fqcn) {
-//
-// 	if (typeof fqcn == "undefined") fqcn = $("#f_elem_city").val();
-//
-// 	let cityfqcn = fqcn;
-//
-// 	if (cityfqcn) {
-//
-// 	    $.getJSON(
-// 	                "http://gd.geobytes.com/GetCityDetails?callback=?&fqcn="+cityfqcn,
-//                      function (data) {
-// 	            $("#geobytesinternet").val(data.geobytesinternet);
-// 	            $("#geobytescountry").val(data.geobytescountry);
-// 	            $("#geobytesregionlocationcode").val(data.geobytesregionlocationcode);
-// 	            $("#geobytesregion").val(data.geobytesregion);
-// 	            $("#geobyteslocationcode").val(data.geobyteslocationcode);
-// 	            $("#geobytescity").val(data.geobytescity);
-// 	            $("#geobytescityid").val(data.geobytescityid);
-// 	            $("#geobytesfqcn").val(data.geobytesfqcn);
-// 	            $("#geobyteslatitude").val(data.geobyteslatitude);
-// 	            $("#geobyteslongitude").val(data.geobyteslongitude);
-// 	            $("#geobytescapital").val(data.geobytescapital);
-// 	            $("#geobytestimezone").val(data.geobytestimezone);
-// 	            $("#geobytesnationalitysingular").val(data.geobytesnationalitysingular);
-// 	            $("#geobytespopulation").val(data.geobytespopulation);
-// 	            $("#geobytesnationalityplural").val(data.geobytesnationalityplural);
-// 	            $("#geobytesmapreference").val(data.geobytesmapreference);
-// 	            $("#geobytescurrency").val(data.geobytescurrency);
-// 	            $("#geobytescurrencycode").val(data.geobytescurrencycode);
-// 	            }
-// 	    );
-// 	}
-// }
-
-
-
-// }
-
-
-
-
   render() {
-    console.log(this.props)
 
      let venues_list = "hi"
     if (this.props.venues) {
@@ -121,6 +66,13 @@ class SearchContainer extends Component {
         return <div>{venue.name}</div>
       })
   }
+
+   let countries = this.state.countries;
+
+  let optionItems = countries.map((country) =>
+                  <option key={country}>{country}</option>
+              );
+
     return (
 
       <div>
@@ -132,40 +84,11 @@ class SearchContainer extends Component {
       />
       {venues_list}
 
-      <div className="section no-pad-bot yellow lighten-3">
-          <div id="show-case" className="container">
-              <div className="section">
-
-                  <div className="row">
-                      <div className="col s12 m12">
-                          <div className="icon-block">
-                              <h2 className="center black-text light header subtitle"><i className="fa fa-file-code-o"></i> <a href="http://battuta.medunes.net" target="_blank">Battuta</a> Show case</h2>
-                              <div className="col s4">
-                                  <label className="left-align" for="country">Country</label>
-                                  <select className="brwoser-default" id="country"></select>
-                              </div>
-
-                              <div className="col s4">
-                                  <label className="left-align" for="region">Region</label>
-                                  <select className="brwoser-default" id="region"></select>
-                              </div>
-
-                              <div className="col s4">
-                                   <label className="left-align" for="city">City</label>
-                                  <select className="autocomplete" id="city"></select>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                   <div className="row">
-                      <div className="col s2 m2"></div>
-                      <div className="col s6 m6">
-                          <div id="location" className="orange lighten-1 black-text"></div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
+      <div>
+             <select>
+                {optionItems}
+             </select>
+         </div>
 
       </div>
 
