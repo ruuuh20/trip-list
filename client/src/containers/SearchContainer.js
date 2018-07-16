75b98c5adcab37656251d20c29c9b290f1bb383e
ruuuh20 <pkim2320@gmail.com>
2018-07-15 22:47:39 -0400

    city workssssss

--- client/src/containers/SearchContainer.js
+++ client/src/containers/SearchContainer.js
@@ -16,8 +16,10 @@ class SearchContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      selectedCountry: "",
       countries: [],
-      regions: []
+      regions: [],
+      cities: []
     }
   }
 
@@ -60,6 +62,14 @@ class SearchContainer extends Component {
 
   handleOption = (e) => {
     let countryCode
+
+//     You have to capture the value of the event target in a variable, and then use that in the updateValue callback.
+//
+// It seems like by the time the callback happens, the target of that specific event is "released"
+      let countryName = e.target.value
+
+
+
     // from stackoverflow - to get the id/code for each country!
     for (let node of e.target.children) {
     if (node.value === e.target.value) {
@@ -68,6 +78,7 @@ class SearchContainer extends Component {
       // return;
     }
   }
+  // console.log(e.target.value + "nameeeee")
 
     let initialRegions = [];
   //
@@ -78,8 +89,7 @@ class SearchContainer extends Component {
   console.log(response)
            return response.json();
        }).then(data => {
-         // console.log(data.body + "Hellllllooooo")
-         // console.log(JSON.stringify(data))
+
          var stringed = JSON.stringify(data)
          console.log(data.length)
 
@@ -87,11 +97,51 @@ class SearchContainer extends Component {
            return region.region
        });
        console.log(initialRegions);
+       // e.persist();
        this.setState({
+          selectedCountry: countryCode,
+
            regions: initialRegions,
        });
    });
   }
+
+  handleRegion = (e) => {
+
+    let regionCode = e.target.value
+    let countryCode = this.state.selectedCountry
+
+    let initialCities = [];
+  //
+   fetchJsonp(  "https://battuta.medunes.net/api/city/"
+ 	    +countryCode
+ 	    +"/search/?region="
+ 	    +regionCode
+ 	    +"&key=bd8c940f6ad6dfa181f7d32922758c74&callback=?")
+   .then((response) => {
+  console.log(response)
+           return response.json();
+       }).then(data => {
+
+         var stringed = JSON.stringify(data)
+         console.log(data.length)
+
+       initialCities = data.map((city) => {
+           return city.city
+       });
+       console.log(initialCities);
+       // e.persist();
+       this.setState({
+
+           cities: initialCities
+       });
+   });
+
+  }
+
+
+
+
 
 
 
@@ -103,29 +153,23 @@ class SearchContainer extends Component {
       venues_list = this.props.venues.map(venue => {
         return <div>{venue.name}</div>
       })
-  }
+    }
 
   //countries 1
-
-   let countries = this.state.countries;
-
+  let countries = this.state.countries;
   let optionItems = countries.map((country) =>
 
               <option
                     key={country.code}
                     data-id={country.code}
-
-                    >{country.value}
+                  >{country.value}
               </option>
-
               );
  // regions 2
-
  let regions = ""
  let regionItems= ""
  if (this.state.regions.length >= 1) {
    regions = this.state.regions
-
  // let regions = this.state.regions ? this.state.regions : ""
  regionItems = regions.map((region) =>
       <option
@@ -137,6 +181,25 @@ class SearchContainer extends Component {
 )
 } else {
   regionItems = "no regions"
+}
+
+
+//cities 3
+let cities = ""
+let cityItems= ""
+if (this.state.cities.length >= 1) {
+  cities = this.state.cities
+// let regions = this.state.regions ? this.state.regions : ""
+cityItems = cities.map((city) =>
+     <option
+         key={city.city}
+         data-id={city.city}
+
+      >{city}
+    </option>
+)
+} else {
+ cityItems = "no city"
 }
 
 
@@ -155,8 +218,11 @@ class SearchContainer extends Component {
              <select onChange={(e) => this.handleOption(e)}>
                 {optionItems}
              </select>
-             <select>
+             <select onChange={this.handleRegion}>
                 {regionItems}
+             </select>
+             <select onChange={this.handleCity}>
+                {cityItems}
              </select>
          </div>
 
